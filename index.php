504ca357ab5c9ba23336a871b77fec9061bc18c3
Nur Azizah Wulandari <151890058+nurazizahwd@users.noreply.github.com>
2024-04-01 10:04:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>LITTERARUM DEPO LIBRARY</title>
+<link rel="stylesheet" href="img/csslogin.css">
+</head>
+<body>
+<center>
+
+<form action="login_proses.php" method="post">
+  <h2> LITTERARUM DEPO LIBRARY</h2>
+  <hr>
+  <div class="imgcontainer">
+    <img src="img/anonymous.png" alt="Avatar" class="avatar">
+  </div>
+
+  <div class="container">
+    <br>
+    <input type="text" placeholder="Enter Username" name="name" required><br>
+
+    <br>
+    <input type="password" placeholder="Enter Password" name="pw" required><br>
+    <br>
+    <button type="submit">Login</button><br>
+</center>
+</form>
+
+</body>
+</html>
